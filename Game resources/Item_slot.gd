7e4e3cd1b883388c4c656e0a7d7a1f9876d5c7d1
Master70000 <themasterprogrammer70000@gmail.com>
2022-08-09 16:08:52 +0530

    Fixed inventory item display

--- Game resources/Item_slot.gd
+++ Game resources/Item_slot.gd
@@ -1,18 +1,14 @@
 extends TextureRect
 
-export var item : PackedScene
-onready var Item
+var item
 
 onready var Sprite = $item
 onready var count = $Count
 onready var description = $Label
 
-func _ready():
-	if item != null:
-		Item = item.instance()
 func _physics_process(_delta):
-	if Item != null:
-		
-		Sprite.texture = Item.sprite
-		count.text = String(Item.count)
-		description.text = Item.Description
+	if item != null:
+
+		Sprite.texture = item.sprite
+		count.text = String(item.count)
+		description.text = item.Description
